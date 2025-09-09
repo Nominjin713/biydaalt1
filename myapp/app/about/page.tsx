a9e53de7f98cab8d5e22e76739ad2f665d6b5917
Nominjin713 <nnominen@gmail.com>
2025-09-09 01:12:15 -0700

    lab2.1

--- myapp/app/about/page.tsx
+++ myapp/app/about/page.tsx
@@ -1,8 +1,9 @@
 import styles from "./page.module.css";
-
+import Header from "@/components/Header";
 export default function About() {
   return (
-    <div className={`p-8 ${styles.customContainer}`}>
+    <div>
+      <Header />
       <h1 className={styles.title}>Minii tuhai</h1>
       <p className={styles.description}>ene huudsiig css module ashiglj hiisn</p>
       <p className="mt-2">bi unshih durtai</p>
