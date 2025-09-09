42a4bf79a602f4710555d61ee7a5fe9d8ba701ed
Nominjin713 <nnominen@gmail.com>
2025-09-09 01:28:55 -0700

    lab2.1

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Layout from "@/components/Layout"
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h4>hello</h4>
+      <Layout> 
+        <h2 className="text-2xl font-bold">Nuur huudas</h2>
+        <p className="mt-4">ene bol layout ashiglasan jishee</p>
+      </Layout>
     </div>
+
   );
 }
