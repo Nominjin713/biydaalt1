7b753af4f021d6843e07de90b0454991ae11abe4
Nominjin713 <nnominen@gmail.com>
2025-09-09 01:42:29 -0700

    lab2.11

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h4>hello</h4>
       <Layout> 
-        <h2 className="text-2xl font-bold">Nuur huudas</h2>
+        <h2 className="text-2xl font-bold"></h2>
         <p className="mt-4">ene bol layout ashiglasan jishee</p>
       </Layout>
     </div>
--- myapp/components/Header.tsx
+++ myapp/components/Header.tsx
@@ -1,5 +1,5 @@
 export default function Header(){
     return(<header className="p-4 bg-blue-600 text-white">
-        <h1 className="text-xl">My web </h1>
+        <h1 className="text-xl">Энэ бол Бямбасүрэн Батзориг намтар </h1>
     </header>)
-}
+}   
