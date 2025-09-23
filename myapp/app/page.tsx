87e32cf6a0e547d123babacc431aa6054184b91f
Nominjin713 <nnominen@gmail.com>
2025-09-23 01:08:06 -0700

    a

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
-import Layout from "@/components/Layout"
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
-    <div >
-      <Layout> 
-        <h2 className="text-2xl font-bold"></h2>
-  
+    <div>
+      <Layout>
+        <h2 className="text-2xl font-bold">Нүүр хуудас</h2>
+
+        {/* Зураг */}
+        <Image
+          src="/images/logo.png"   // public/images/logo.png
+          alt="Манай сайтын лого"
+          width={300}
+          height={200}
+        />
       </Layout>
     </div>
   );
